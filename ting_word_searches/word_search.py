cc2df4ac8a54cf5b7948dd80e352afbae8b10edc
Enei <eneipereira@outlook.com.br>
2022-12-02 16:04:12 -0300

    feat: Implementa a função search_by_word, que busca uma palavra em todos os arquivos processados.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -7,6 +7,19 @@ def get_occurences(word, items):
             })
     
     return ocurrences
+
+
+def get_lines_and_content(word, items):
+    ocurrences = []
+    for index in range(len(items)):
+        if word.lower() in items[index].lower():
+            ocurrences.append({
+                "linha": index + 1,
+                "conteudo": items[index]
+            })
+    
+    return ocurrences
+    
     
     
 def exists_word(word, instance):
@@ -29,4 +42,17 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    dict_list_item = []
+
+    for index in range(instance.__len__()):
+        matched_item = instance.search(index)
+        ocurrences_list = get_lines_and_content(word, matched_item["linhas_do_arquivo"])
+        
+        if ocurrences_list:
+            dict_list_item.append({
+                "palavra": word,
+                "arquivo": matched_item["nome_do_arquivo"],
+                "ocorrencias": ocurrences_list
+            })
+        
+    return dict_list_item
