fac45ee1cf5d0f70112680760eaaaac35f5fbfbd
Enei <eneipereira@outlook.com.br>
2022-12-02 00:38:11 -0300

    feat: Implementa uma fila, que permite inserir, remover e buscar um elemento.

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,27 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.size()
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.is_empty():
+            return None
+        
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > self.size() - 1:
+            raise IndexError
+        
+        return self._data[index]
+    
+    def size(self):
+        return len(self._data)
+    
+    def is_empty(self):
+        return not bool(self._data)
