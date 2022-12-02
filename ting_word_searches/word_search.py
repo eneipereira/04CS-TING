7d502a4a5ffcdfbe81d250f8de4e2473f47b086a
Enei <eneipereira@outlook.com.br>
2022-12-02 16:03:38 -0300

    feat: Implementa a função exists_word, que verifica a existência de uma palavra em todos os arquivos processados.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,31 @@
+def get_occurences(word, items):
+    ocurrences = []
+    for index in range(len(items)):
+        if word.lower() in items[index].lower():
+            ocurrences.append({
+                "linha": index + 1
+            })
+    
+    return ocurrences
+    
+    
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    dict_list_item = []
+
+    for index in range(instance.__len__()):
+        matched_item = instance.search(index)
+        ocurrences_list = get_occurences(word, matched_item["linhas_do_arquivo"])
+        
+        if ocurrences_list:
+            dict_list_item.append({
+                "palavra": word,
+                "arquivo": matched_item["nome_do_arquivo"],
+                "ocorrencias": ocurrences_list
+            })
+        
+    return dict_list_item
+    
+    
 
 
 def search_by_word(word, instance):
