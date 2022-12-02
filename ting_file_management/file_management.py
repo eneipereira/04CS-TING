55be89d168c6e816797f232e12d6e42f4205a01d
Enei <eneipereira@outlook.com.br>
2022-12-02 00:39:20 -0300

    feat: Implementa a função 'txt_importer', que retorna uma lista contendo as linhas de um arquivo txt.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+
+    try:
+        with open(path_file, 'r') as file:
+           read_file = file.read()
+           file_lines_list = read_file.split('\n')
+            
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+        
+    else:
+        return file_lines_list
